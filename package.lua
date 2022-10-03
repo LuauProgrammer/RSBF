dfb083f8f9723cbfa4bfda5e5fd85cdc988b08ee
LuauProgrammer <aydentaylorl@gmail.com>
2022-10-03 19:59:56 -0400

    packages

---
+++ package.lua
@@ -0,0 +1,16 @@
+return {
+  name = "RSBF",
+  version = "0.0.1",
+  description = "Really Simple Bot Framework.",
+  tags = { "lua", "lit", "luvit" },
+  license = "MIT",
+  author = { name = "LuauProgrammer" },
+  homepage = "https://github.com/LuauProgrammer/RSBF/",
+  dependencies = {
+    "SinisterRectus/discordia"
+  },
+  files = {
+    "**.lua",
+    "!test*"
+  }
+}
